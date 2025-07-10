a3967b0cffd8e2b7a5df7c93484c8778ba78ac6f
Ghady Abboud <abboudghady@gmail.com>
2025-07-10 01:21:35 -0400

    Set up first api endpoint

--- server/main.go
+++ server/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/joho/godotenv"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
@@ -11,5 +13,21 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	fmt.Println("Project Init")
+
+	router := gin.Default()
+	router.Use(cors.Default())
+	router.GET("/quote", getQuote)
+
+	err = router.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getQuote(c *gin.Context) {
+	symbol := c.DefaultQuery("symbol", "")
+	c.JSON(200, gin.H {
+		"symbol" : symbol,
+		"price": 100.0,
+	})
 }

3d1904080d6eeacf5490ca736ccbde0cc3154742
Ghady Abboud <abboudghady@gmail.com>
2025-07-13 16:34:10 -0400

    Init Alpaca Endpoint

--- server/main.go
+++ server/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"os"
+	//"strings"
 
-	"github.com/joho/godotenv"
-	"github.com/gin-gonic/gin"
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/go-resty/resty/v2"
+	"github.com/joho/godotenv"
 )
+
+var client *resty.Client
+
+func initResty() {
+	client = resty.New().SetBaseURL(os.Getenv("ALPACA_MARKET_URL")).SetHeader("APCA-API-KEY-ID", os.Getenv("ALPACA_API_KEY")).SetHeader("APCA-API-SECRET-KEY",os.Getenv("ALPACA_SECRET_KEY"))
+}
 
 func main() {
 	err := godotenv.Load()
@@ -14,9 +25,11 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	initResty()
+
 	router := gin.Default()
 	router.Use(cors.Default())
-	router.GET("/quote", getQuote)
+	router.GET("/get-quote", getQuote)
 
 	err = router.Run(":8080")
 	if err != nil {
@@ -25,9 +38,13 @@ func main() {
 }
 
 func getQuote(c *gin.Context) {
-	symbol := c.DefaultQuery("symbol", "")
-	c.JSON(200, gin.H {
-		"symbol" : symbol,
-		"price": 100.0,
-	})
+	endpointUrl := fmt.Sprintf("%s/stocks/quotes/latest", os.Getenv("ALPACA_MARKET_URL"))
+
+	resp, err := client.R().SetDebug(true).SetQueryParam("symbols", "AAPL").Get(endpointUrl)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch data"})
+		return
+	}
+
+	c.Data(resp.StatusCode(), "application/json", resp.Body())
 }
